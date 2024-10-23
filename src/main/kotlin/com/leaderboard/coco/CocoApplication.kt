69091f7cc2a82a71bc38dce91e8140058f98681a
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2024-10-23 23:36:57 +0900

    init

---
+++ src/main/kotlin/com/leaderboard/coco/CocoApplication.kt
@@ -0,0 +1,11 @@
+package com.leaderboard.coco
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CocoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CocoApplication>(*args)
+}
