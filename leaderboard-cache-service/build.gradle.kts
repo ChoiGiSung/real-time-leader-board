c03fafd594c41b8d20b01034eb6bc052d00ce3c4
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2024-10-27 12:43:26 +0900

    캐시하고 리더보드 수정 메시지 발행

--- leaderboard-cache-service/build.gradle.kts
+++ leaderboard-cache-service/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
-
 }
